82cf620962c8cc9f4a7678995e4c851590c39891
Noah Milberger <n.milberger@gmail.com>
2023-03-20 08:32:48 -0400

    Initial commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  win.loadURL("https://telepathy-game.vercel.app/");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
